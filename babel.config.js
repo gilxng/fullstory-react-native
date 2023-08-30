ffc0477262bcc00d4b51b5b1ab6e67c70e0aa56b
gilxng <gilang@devhaus.com.sg>
2023-08-30 11:48:45 +0700

    feat: Implement FullStory on Android

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@fullstory/react-native',
+    ['@fullstory/babel-plugin-annotate-react', {'annotate-fragments': true}],
+  ],
 };
